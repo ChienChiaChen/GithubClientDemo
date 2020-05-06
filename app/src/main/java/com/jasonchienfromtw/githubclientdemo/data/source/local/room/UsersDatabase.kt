919ab14d51350cf6da7f91b26537519fd3882ac5
JasonChien <jasonchien@kkstream.com>
2020-05-07 05:08:12 +0800

    Add db module.

--- app/src/main/java/com/jasonchienfromtw/githubclientdemo/data/source/local/room/UsersDatabase.kt
+++ app/src/main/java/com/jasonchienfromtw/githubclientdemo/data/source/local/room/UsersDatabase.kt
@@ -5,6 +5,8 @@ import androidx.room.RoomDatabase
 import com.jasonchienfromtw.githubclientdemo.data.constants.RepositoryConstants.DATABASE_VERSION
 import com.jasonchienfromtw.githubclientdemo.data.source.local.room.entity.UserEntity
 
+const val DATABASE_NAME = "github_users_database"
+
 @Database(entities = [UserEntity::class], version = DATABASE_VERSION)
 abstract class UsersDatabase: RoomDatabase() {
     abstract fun usersDao(): UsersDao
