6fc43e33a3c1748c0e10365cdcc4e67ae5b35a62
JasonChien <jasonchien@kkstream.com>
2020-05-07 10:27:40 +0800

    Add domain module.

--- app/src/main/java/com/jasonchienfromtw/githubclientdemo/presentation/di/app/AppComponent.kt
+++ app/src/main/java/com/jasonchienfromtw/githubclientdemo/presentation/di/app/AppComponent.kt
@@ -3,6 +3,7 @@ package com.jasonchienfromtw.githubclientdemo.presentation.di.app
 import com.jasonchienfromtw.githubclientdemo.App
 import com.jasonchienfromtw.githubclientdemo.presentation.di.data.LocalDataModule
 import com.jasonchienfromtw.githubclientdemo.presentation.di.data.RemoteDataModule
+import com.jasonchienfromtw.githubclientdemo.presentation.di.domain.DomainModule
 import com.jasonchienfromtw.githubclientdemo.presentation.di.main.MainActivityInjector
 import com.jasonchienfromtw.githubclientdemo.presentation.di.viewmodel.ViewModelModule
 import dagger.Component
@@ -17,7 +18,8 @@ import javax.inject.Singleton
         MainActivityInjector::class,
         ViewModelModule::class,
         RemoteDataModule::class,
-        LocalDataModule::class
+        LocalDataModule::class,
+        DomainModule::class
     ]
 )
 @Singleton
---
+++ app/src/main/java/com/jasonchienfromtw/githubclientdemo/presentation/di/domain/DomainModule.kt
@@ -0,0 +1,17 @@
+package com.jasonchienfromtw.githubclientdemo.presentation.di.domain
+
+import com.jasonchienfromtw.githubclientdemo.domain.repository.UsersRepository
+import com.jasonchienfromtw.githubclientdemo.domain.usecase.GetUserListUseCase
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+
+@Module
+class DomainModule {
+
+    @Singleton
+    @Provides
+    internal fun provideGetUserListUseCase(usersRepository: UsersRepository): GetUserListUseCase =
+        GetUserListUseCase(usersRepository)
+
+}
